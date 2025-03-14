c2bd7942d81cd7d451d346ed66a7621481c60086
Mohammad Kasim <ka000sim@gmail.com>
2025-03-14 10:41:33 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, Dosto!!!!!")
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	// comma ok || err ok
+	fmt.Println("Enter the rating for our pizza: ")
+	// cmma ok || err ok
+	input, _ := reader.ReadString('\n') // try or catch part
+	// comma ok || err ok
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of this rating is %T", input)
 }
